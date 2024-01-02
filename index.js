f5db719bed34d3cbfd2e7408bfe2c966afdd83bc
frostrhea <84138604+frostrhea@users.noreply.github.com>
2024-01-02 03:57:17 +0000

    feat: implement API POST summarize request functionality

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const summarizeText = require('./summarize.js');
 
 // Parses JSON bodies (as sent by API clients)
 app.use(express.json());
 
 // Serves static files from the 'public' directory
 app.use(express.static('public'));
+
+app.post('/summarize', (req, res) => {
+  const text = req.body.text;  //get text from request body
+
+  summarizeText(text);  //call function passing in text
+    .then(response => {
+      res.send(response);  // Send the summary text as a response to the client
+    })
+    .catch(error => {
+      console.log(error.messaged);
+    });
+});
 
 // Start the server
 app.listen(port, () => {
---
+++ summarize.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+// This is the function where the call to the API is made. Returns the summarized text as a string.
+async function summarizeText(text){
+  
+  let data = JSON.stringify({
+    "inputs": text,
+    "parameters": {
+      "max_length": 100,
+      "min_length": 30
+    }
+  });
+
+  // A config object that will contain the instructions for the API call
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': 'Bearer' + process.env['ACCESS_TOKEN']
+    },
+    data : data
+  };
+
+
+  try {
+    const response = await axios.request(config);
+    return response.data[0].summary_text;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = summarizeText;
